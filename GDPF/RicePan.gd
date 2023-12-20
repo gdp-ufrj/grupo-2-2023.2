65e04d1226f15a44ca57c64906aa1db83024df7e
EduardoMeloC <33487458+EduardoMeloC@users.noreply.github.com>
2023-12-20 15:45:30 -0300

    Simplified State Machine Logic

---
+++ GDPF/RicePan.gd
@@ -0,0 +1,74 @@
+extends Node2D
+class_name StateMachine
+
+var _current_state_name = "EmptyState"
+
+@onready var _pan : Pan = $"../"
+@onready var _animator: AnimatedSprite2D = _pan.get_node("AnimatedSprite2D")
+@onready var _timer : Timer = _pan.get_node("Timer")
+
+var States = {
+	"EmptyState": {
+		"on_ingredient_added": 
+			func(ingredient): 
+				if(ingredient == "rice"): 
+					_switch_to_state("RiceState")
+				if(ingredient == "water"):
+					_switch_to_state("WaterState"),
+	},
+	"RiceState": {
+		"on_ingredient_added":
+			func(ingredient_name):
+				if(ingredient_name == "water"):
+					_switch_to_state("RiceAndWaterState"),
+	},
+	"WaterState": {
+		"on_ingredient_added":
+			func(ingredient_name):
+				if(ingredient_name == "rice"):
+					_switch_to_state("RiceAndWaterState"),
+	},
+	"RiceAndWaterState": {
+		"on_fire_start":
+			func():
+				_switch_to_state("CookingState"),
+	},
+	"CookingState": {
+		"timer_wait": 5,
+		"on_fire_stop":
+			func():
+				_switch_to_state("RiceAndWaterState"),
+		"on_timer_timeout":
+			func():
+				_switch_to_state("CookedState"),
+	},
+	"CookedState": {
+		"timer_wait": 5,
+		"on_timer_timeout":
+			func():
+				_switch_to_state("OvercookedState"),
+				
+	},
+}
+
+func _ready():
+	_switch_to_state(_current_state_name)
+
+func _switch_to_state(next_state_name):
+	var current_state = States.get(_current_state_name)
+	var next_state = States.get(next_state_name)
+	_animator.play(next_state_name)
+	
+	if(current_state.get("on_ingredient_added")):
+		_pan.ingredient_released_on_pan.disconnect(current_state.get("on_ingredient_added"))
+	if(next_state.get("on_ingredient_added")):
+		_pan.ingredient_released_on_pan.connect(next_state.get("on_ingredient_added"))
+		
+	if(current_state.get("on_timer_timeout")):
+		_timer.timeout.disconnect(current_state.get("on_timer_timeout"))
+	if(next_state.get("on_timer_timeout")):
+		_timer.wait_time = next_state.get("timer_wait")
+		_timer.timeout.connect(next_state.get("on_timer_timeout"))
+		
+	_current_state_name = next_state_name
+
--- GDPF/rice_pot/pan.gd
+++ GDPF/rice_pot/pan.gd
@@ -9,33 +9,34 @@ signal ingredient_released_on_pan(ingredient, pan)
 
 var _is_mouse_inside = false
 
-
-@onready var fsm = $FiniteStateMachine as FiniteStateMachine
-@onready var empty_state = $FiniteStateMachine/EmptyState as EmptyState
-@onready var rice_state = $FiniteStateMachine/RiceState as RiceState
-@onready var water_state = $FiniteStateMachine/WaterState as WaterState
-@onready var cooking_state = $FiniteStateMachine/CookingState as CookingState
-@onready var cooked_state = $FiniteStateMachine/CookedState as CookedState
-@onready var overcooked_state = $FiniteStateMachine/OvercookedState as OvercookedState
-
-
-func _create_transition(state_signal: Signal, next_state: State):
-	state_signal.connect(fsm.change_state.bind(next_state))
-
-func _ready():
-	_create_transition(empty_state.rice_dropped, rice_state)
-	_create_transition(empty_state.water_dropped, water_state)
-	_create_transition(rice_state.water_dropped, cooking_state)
-	_create_transition(water_state.rice_dropped, cooking_state)
-	_create_transition(cooking_state.cooking_finished, cooked_state)
-	_create_transition(cooked_state.cooked_finished, overcooked_state)
+#
+#@onready var fsm = $FiniteStateMachine as FiniteStateMachine
+#@onready var empty_state = $FiniteStateMachine/EmptyState as EmptyState
+#@onready var rice_state = $FiniteStateMachine/RiceState as RiceState
+#@onready var water_state = $FiniteStateMachine/WaterState as WaterState
+#@onready var cooking_state = $FiniteStateMachine/CookingState as CookingState
+#@onready var cooked_state = $FiniteStateMachine/CookedState as CookedState
+#@onready var overcooked_state = $FiniteStateMachine/OvercookedState as OvercookedState
+#
+#
+#func _create_transition(state_signal: Signal, next_state: State):
+#	state_signal.connect(fsm.change_state.bind(next_state))
+#
+#func _ready():
+#	_create_transition(empty_state.rice_dropped, rice_state)
+#	_create_transition(empty_state.water_dropped, water_state)
+#	_create_transition(rice_state.water_dropped, cooking_state)
+#	_create_transition(water_state.rice_dropped, cooking_state)
+#	_create_transition(cooking_state.cooking_finished, cooked_state)
+#	_create_transition(cooked_state.cooked_finished, overcooked_state)
 
 func _on_ingredient_released(ingredient):
-	if (recipes.has(ingredient) && _is_mouse_inside):
-		ingredient_released_on_pan.emit(ingredient, pan_name)
+	if (_is_mouse_inside):
+		ingredient_released_on_pan.emit(ingredient)
 
 func _on_area_2d_mouse_entered():
 	_is_mouse_inside = true
 	
 func _on_area_2d_mouse_exited():
 	_is_mouse_inside = false
+
